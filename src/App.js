b118ff1cd008fcefc7b761b943048e90861373f1
Atisha25 <atisha.gusain@nineleaps.com>
2020-06-12 10:53:33 +0530

    Atisha | initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          namaste india!!
         </p>
         <a
           className="App-link"
